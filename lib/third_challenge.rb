3ba67b09fd16efe267fbeaf7ff7f6b34b59d3e0e
Daniel Andrew <dwandrew@hotmail.com>
2020-02-18 10:41:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,23 +7,23 @@ def third_challenge
         },
       :matriarch => {
         :name =>"Lady Montague",
-        :age =>
+        :age =>"54"
         },
       :hero => {
-        :name =>,
-        :age =>,
+        :name =>"Romeo",
+        :age =>"15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name =>,
-        :age =>
+        :name =>"Lord Capulet",
+        :age =>"50"
         },
       :matriarch => {
-        :name =>,
-        :age =>
+        :name =>"Lady Capulet",
+        :age =>"51"
         },
       :heroine => {
         :name =>,
